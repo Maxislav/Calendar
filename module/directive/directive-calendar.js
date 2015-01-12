fee3f271003919d95977dc6220aa9b922a36406f
mars <lmaxim@mail.ru>
2015-01-12 19:00:21 +0200

    htформат

--- module/directive/directive-calendar.js
+++ module/directive/directive-calendar.js
@@ -1,186 +1,201 @@
-calendarModule.directive('calendar', ['$compile', '$templateCache', 'constantCalendar','$timeout', function ($compile, $templateCache, constantCalendar, $timeout) {
-	function formatMonth(date, _after, _before) {
-		var dateNowValue = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()).getTime();
-		var afterValue = _after.getTime();
-		var beforeValue = _before.getTime();
+calendarModule.directive('calendar', ['$compile', '$templateCache', 'constantCalendar', '$timeout', 'factoryOffset', function ($compile, $templateCache, constantCalendar, $timeout, factoryOffset) {
+    function formatMonth(date, _after, _before) {
+        var dateNowValue = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()).getTime();
+        var afterValue = _after.getTime();
+        var beforeValue = _before.getTime();
 
-		function checkCurrentDate(d) {
-			var value = ''
-			if (dateNowValue == d.getTime()) {
-				value += 'current'
-			}
-			if (afterValue <= d.getTime() && d.getTime() <= beforeValue) {
-				value += ' select'
-			}
+        function checkCurrentDate(d) {
+            var value = ''
+            if (dateNowValue == d.getTime()) {
+                value += 'current'
+            }
+            if (afterValue <= d.getTime() && d.getTime() <= beforeValue) {
+                value += ' select'
+            }
 
-			return value
-		}
+            return value
+        }
 
-		var array = [];
-		for (var i = 0; i < 50; i++) {
-			var d = new Date(date.getFullYear(), date.getMonth(), i)
-			if (d.getMonth() == date.getMonth()) {
-				array.push({
-					date: d,
-					value: d.getTime(),
-					dayWeek: d.getDay(),
-					class: checkCurrentDate(d)
-				})
-			}
-		}
-		var fillBefore = (array[0].dayWeek == 0) ? 6 : array[0].dayWeek - 1;
-		var fillAfter = (array[array.length - 1].dayWeek == 0) ? 0 : 7 - array[array.length - 1].dayWeek;
-		for (var i = 0; i < fillBefore; i++) {
-			array.unshift(null)
-		}
-		for (var i = 0; i < fillAfter; i++) {
-			array.push(null)
-		}
+        var array = [];
+        for (var i = 0; i < 50; i++) {
+            var d = new Date(date.getFullYear(), date.getMonth(), i)
+            if (d.getMonth() == date.getMonth()) {
+                array.push({
+                    date: d,
+                    value: d.getTime(),
+                    dayWeek: d.getDay(),
+                    class: checkCurrentDate(d)
+                })
+            }
+        }
+        var fillBefore = (array[0].dayWeek == 0) ? 6 : array[0].dayWeek - 1;
+        var fillAfter = (array[array.length - 1].dayWeek == 0) ? 0 : 7 - array[array.length - 1].dayWeek;
+        for (var i = 0; i < fillBefore; i++) {
+            array.unshift(null)
+        }
+        for (var i = 0; i < fillAfter; i++) {
+            array.push(null)
+        }
 
-		var rows = parseFloat((array.length / 7).toFixed(0));
-		var arrMonth = [];
-		var k = 0
-		for (var i = 0; i < rows; i++) {
-			arrMonth[i] = [];
-			for (var d = 0; d < 7; d++) {
-				arrMonth[i].push(array[k])
-				k++;
-			}
+        var rows = parseFloat((array.length / 7).toFixed(0));
+        var arrMonth = [];
+        var k = 0
+        for (var i = 0; i < rows; i++) {
+            arrMonth[i] = [];
+            for (var d = 0; d < 7; d++) {
+                arrMonth[i].push(array[k])
+                k++;
+            }
 
-		}
-		//  console.log(arrMonth)
-		return arrMonth
-	}
-
-
-	return {
-		restrict: 'EA',
-		replace: true,
-		templateUrl: 'module/partials/calendar-label.html',
-		scope: {
-			before: '=before',
-			after: '=after',
-			link: '@link'
-		},
-		controller: ['$scope', '$element', '$attrs', '$timeout', '$templateCache', function ($scope, $element, $attrs, $timeout, $templateCache) {
-			var link = $scope.link == 'true' ? true : false ;
-
-			$scope.show = false;
-			$scope.constantCalendar = constantCalendar;
-
-			$scope.beforeLabelValue =  $scope.before;
-			$scope.afterLabelValue =  $scope.after;
-
-			var currentViewDate = new Date($scope.after.getTime() + (($scope.before.getTime() - $scope.after.getTime()) / 2));   // $scope.date;
-			var previousMonth, nextMonth;
-			$scope.includeMonths = 'module/partials/container-months.html'
-
-			function render(date) {
-				previousMonth = new Date(date.getFullYear(), date.getMonth() - 1, date.getDate())
-				nextMonth = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
+        }
+        //  console.log(arrMonth)
+        return arrMonth
+    }
 
 
+    return {
+        restrict: 'EA',
+        replace: true,
+        templateUrl: 'module/partials/calendar-label.html',
+        scope: {
+            before: '=before',
+            after: '=after',
+            link: '@link'
+        },
+        controller: ['$scope', '$element', '$attrs', '$timeout', '$templateCache', function ($scope, $element, $attrs, $timeout, $templateCache) {
+            var link = $scope.link == 'true' ? true : false;
+
+            $scope.show = false;
+            $scope.constantCalendar = constantCalendar;
+
+            $scope.months = [];
+
+            $scope.beforeLabelValue = $scope.before;
+            $scope.afterLabelValue = $scope.after;
+
+            var currentViewDate = new Date($scope.after.getTime() + (($scope.before.getTime() - $scope.after.getTime()) / 2));   // $scope.date;
+            var previousMonth, nextMonth;
+            $scope.includeMonths = 'module/partials/container-months.html'
+
+            function render(date) {
+                previousMonth = new Date(date.getFullYear(), date.getMonth() - 1, date.getDate())
+                nextMonth = new Date(date.getFullYear(), date.getMonth() + 1, date.getDate());
+
+                $scope.months[0] = {
+                    formatMonth: formatMonth(previousMonth, $scope.after, $scope.before),
+                    value: previousMonth.getTime(),
+                    btn: 'module/partials/btn-back.html'
+                };
+
+                $scope.months[1] = {
+                    formatMonth: formatMonth(date, $scope.after, $scope.before),
+                    value: date.getTime(),
+                    btn: null
+                }
 
 
-				$scope.arrMonth = formatMonth(date, $scope.after, $scope.before);
-				$scope.arrMonth.value = date.getTime();
+                $scope.months[2] = {
+                    formatMonth: formatMonth(nextMonth, $scope.after, $scope.before),
+                    value: nextMonth.getTime(),
+                    btn: 'module/partials/btn-forward.html'
+                }
+            }
 
-				$scope.arrMonthBefore = formatMonth(previousMonth, $scope.after, $scope.before);
-				$scope.arrMonthBefore.value = previousMonth.getTime();
+            render(currentViewDate);
 
-				$scope.arrMonthAfter = formatMonth(nextMonth, $scope.after, $scope.before);
-				$scope.arrMonthAfter.value = nextMonth.getTime();
-			}
+            $scope.stepBack = function () {
+                var date = new Date(currentViewDate.getFullYear(), currentViewDate.getMonth() - 1, currentViewDate.getDate())
+                currentViewDate = date;
+                render(date);
+            }
+            $scope.stepForward = function () {
 
-			render(currentViewDate);
+                var date = new Date(currentViewDate.getFullYear(), currentViewDate.getMonth() + 1, currentViewDate.getDate())
+                currentViewDate = date
+                render(date)
 
-			$scope.stepBack = function () {
-				var date = new Date(currentViewDate.getFullYear(), currentViewDate.getMonth() - 1, currentViewDate.getDate())
-				currentViewDate = date;
-				render(date);
-			}
-			$scope.stepForward = function () {
+            }
+            $scope.selectEvent = function (day) {
+                if ($scope.before.getTime() < day.value) {
+                    $scope.before = new Date(day.value)
 
-				var date = new Date(currentViewDate.getFullYear(), currentViewDate.getMonth() + 1, currentViewDate.getDate())
-				currentViewDate = date
-				render(date)
+                } else if (day.value < $scope.after.getTime()) {
+                    $scope.after = new Date(day.value)
+                } else {
+                    $scope.after = new Date(day.value)
+                    $scope.before = new Date(day.value)
+                }
+            }
 
-			}
-			$scope.selectEvent = function (day) {
-				if ($scope.before.getTime() < day.value) {
-					$scope.before = new Date(day.value)
+            $scope.apply = function () {
+                $scope.click()
+            }
 
-				} else if (day.value < $scope.after.getTime()) {
-					$scope.after = new Date(day.value)
-				} else {
-					$scope.after = new Date(day.value)
-					$scope.before = new Date(day.value)
-				}
+            $scope.$watch('before', function () {
+                render(currentViewDate)
+            })
+            $scope.$watch('after', function () {
+                render(currentViewDate)
+            })
 
-			}
+            function listen(e) {
+                var val = false
+                angular.forEach($element.find('*'), function (el) {
+                    if (angular.equals(angular.element(el), angular.element(e.target))) {
+                        val = true
+                        return
+                    }
+                })
+                if (!val) {
+                    ($scope.show = false)
+                    $scope.$apply()
+                }
+            }
 
-			$scope.apply = function(){
-				$scope.click()
-			}
+            function cleanup() {
+                window.document.removeEventListener('click', listen);
+            }
 
-			$scope.$watch('before', function () {
-				render(currentViewDate)
-			})
-			$scope.$watch('after', function () {
-				render(currentViewDate)
-			})
+            $scope.$watch('show', function (val, newVal) {
+                if (val) {
+                    window.document.addEventListener('click', listen);
+                    //console.log(factoryOffset.getOffset($scope.content[0]).top)
 
-			function listen(e) {
-				var val = false
-				angular.forEach($element.find('*'), function (el) {
-					if (angular.equals(angular.element(el), angular.element(e.target))) {
-						val = true
-						return
-					}
-				})
-				if (!val) {
-					($scope.show = false)
-					$scope.$apply()
-				}
-			}
+                } else {
+                    window.document.removeEventListener('click', listen);
+                }
+            });
+            $scope.$on('$destroy', function () {
+                console.log("destroy");
+                cleanup();
+            });
+        }],
+        link: function ($scope, $element) {
+            var content = null;
+            $scope.show = false;
+            $scope.content;
+            function init() {
+                var linkFn = $compile($templateCache.get('module/partials/calendar-view.html'));
+                $scope.content = content = linkFn($scope);
 
-			function cleanup() {
-				window.document.removeEventListener('click', listen);
-			}
+                content.css('display', 'none')
+                $element.append(content);
+                console.log(factoryOffset.getOffset($element[0]).top)
+                $timeout(function () {
+                    $scope.show = true
+                    content.css('display', 'inherit')
+                }, 1)
+            }
 
-			$scope.$watch('show', function (val, newVal) {
-				if (val) {
-					window.document.addEventListener('click', listen)
-				} else {
-					window.document.removeEventListener('click', listen);
-				}
-			});
-			$scope.$on('$destroy', function () {
-				console.log("destroy");
-				cleanup();
-			});
-		}],
-		link: function ($scope, $element) {
-			var content = null;
-			$scope.show = false;
-			function init() {
-				var linkFn = $compile($templateCache.get('module/partials/calendar-view.html'));
-				content = linkFn($scope);
-				content.css('display','none')
-				$element.append(content);
-				$timeout(function(){
-					$scope.show = true
-					content.css('display','inherit')
-				},1)
-			}
-			$scope.click = function () {
-				if (!content) {
-					init()
-					return
-				}
-				$scope.show = !$scope.show
-			}
-		}
+            $scope.click = function () {
+                if (!content) {
+                    init()
+                    return
+                }
+                console.log(factoryOffset.getOffset($element[0]).top)
+                $scope.show = !$scope.show
+            }
+        }
 
-	}
+    }
 }])
--- module/partials/template.js
+++ module/partials/template.js
@@ -1,6 +1,16 @@
 angular.module('calendarModule').run(['$templateCache', function($templateCache) {
   'use strict';
 
+  $templateCache.put('module/partials/btn-back.html',
+    "<div class=\"btn-back\" ng-click=\"stepBack()\">&#8249;</div>"
+  );
+
+
+  $templateCache.put('module/partials/btn-forward.html',
+    "<div class=\"btn-forward\" ng-click=\"stepForward()\">&#8250;</div>"
+  );
+
+
   $templateCache.put('module/partials/calendar-days.html',
     "<div class=\"row days\"><div class=\"col-day text-right\" ng-repeat=\"days in constantCalendar.days track by $index\"><span ng-switch=\"$index\"><span ng-switch-when=\"6\" class=\"red\">{{days}}</span> <span ng-switch-when=\"5\" class=\"red\">{{days}}</span> <span ng-switch-default>{{days}}</span></span></div></div>"
   );
@@ -17,7 +27,7 @@ angular.module('calendarModule').run(['$templateCache', function($templateCache)
 
 
   $templateCache.put('module/partials/container-months.html',
-    "<table><tr><td><div class=\"block-month\"><daysweek></daysweek><div class=\"border-line\"><div class=\"btn-back\" ng-click=\"stepBack()\">&#8249;</div></div><div class=\"row name-month\">{{arrMonthBefore.value | date:'MMMM yyyy'}}</div><div class=\"numeric\"><div class=\"row\" ng-repeat=\"week in arrMonthBefore track by $index\"><div class=\"col-day text-right {{day.class}}\" ng-repeat=\"day in arrMonthBefore[$index] track by $index\" ng-click=\"selectEvent(day)\"><span ng-switch=\"$index\"><span ng-switch-when=\"6\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-when=\"5\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-default>{{day.value | date:'d'}}</span></span></div></div></div></div></td><td><!--текущий--><div class=\"block-month\"><daysweek></daysweek><div class=\"border-line\"></div><div class=\"row name-month\">{{arrMonth.value | date:'MMMM yyyy'}}</div><div class=\"numeric\"><div class=\"row\" ng-repeat=\"week in arrMonth track by $index\"><div class=\"col-day text-right {{day.class}}\" ng-repeat=\"day in arrMonth[$index] track by $index\" ng-click=\"selectEvent(day)\"><span ng-switch=\"$index\"><span ng-switch-when=\"6\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-when=\"5\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-default>{{day.value | date:'d'}}</span></span></div></div></div></div></td><td><!--Сдедующий--><div class=\"block-month\"><daysweek></daysweek><div class=\"border-line\"><div class=\"btn-forward\" ng-click=\"stepForward()\">&#8250;</div></div><div class=\"row name-month\">{{arrMonthAfter.value | date:'MMMM yyyy'}}</div><div class=\"numeric\"><div class=\"row\" ng-repeat=\"week in arrMonthAfter track by $index\"><div class=\"col-day text-right {{day.class}}\" ng-repeat=\"day in arrMonthAfter[$index] track by $index\" ng-click=\"selectEvent(day)\"><span ng-switch=\"$index\"><span ng-switch-when=\"6\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-when=\"5\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-default>{{day.value | date:'d'}}</span></span></div></div></div></div></td></tr></table>"
+    "<table><tr><td ng-repeat=\"month in months track by $index\"><!--текущий--><div class=\"block-month\"><daysweek></daysweek><div class=\"border-line\" ng-include=\"month.btn\"></div><div class=\"row name-month\">{{month.value | date:'MMMM yyyy'}}</div><div class=\"numeric\"><div class=\"row\" ng-repeat=\"week in month.formatMonth track by $index\"><div class=\"col-day text-right {{day.class}}\" ng-repeat=\"day in month.formatMonth[$index] track by $index\" ng-click=\"selectEvent(day)\"><span ng-switch=\"$index\"><span ng-switch-when=\"6\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-when=\"5\" class=\"red\">{{day.value | date:'d'}}</span> <span ng-switch-default>{{day.value | date:'d'}}</span></span></div></div></div></div></td></tr></table>"
   );
 
 }]);
