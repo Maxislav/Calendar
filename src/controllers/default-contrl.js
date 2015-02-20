c29c46a01722f6de17dd9f9e56b32f257a3bd911
mars <lmaxim@mail.ru>
2015-02-20 11:38:04 +0200

    внедрение exact

--- src/controllers/default-contrl.js
+++ src/controllers/default-contrl.js
@@ -1,12 +1,12 @@
 app.controller('defaultContrl', ['$scope', '$interval',function($scope, $interval){
 	var date = new Date();
 
-	$scope.date = new Date();
+	//$scope.date = new Date();
 
-	$scope.before = new Date();
+	//$scope.before = new Date();
 
 	var d = new Date()
-	$scope.after = new Date(d.getFullYear(), d.getMonth(), d.getDate()-4)
+	//$scope.after = new Date(d.getFullYear(), d.getMonth(), d.getDate()-4)
 
 	$scope.exact = new Date();
 
